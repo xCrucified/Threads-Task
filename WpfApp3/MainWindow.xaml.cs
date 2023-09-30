22a8760d3b89462b7dd34bc98799aa4275d22a1c
zheni4ka <ebranik15@gmail.com>
2023-09-30 22:28:13 +0300

    1234

--- WpfApp3/MainWindow.xaml.cs
+++ WpfApp3/MainWindow.xaml.cs
@@ -17,36 +17,160 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp3
+namespace ThreadsandTokens
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    /// 
-
-    public class Alpha
-    {
-        public KeyValuePair<int, int> Nums { get; set; }
-        public CancellationToken Token { get; init; }
-
-        public Alpha(KeyValuePair<int, int> tmp, CancellationToken token)
-        {
-            Nums = tmp;
-            Token = token;
-        }
-    }
-
-
     public partial class MainWindow : Window
     {
-        public static int fibonacci(int number)
+        
+        CancellationTokenSource cts = new CancellationTokenSource();
+        CancellationTokenSource cts2 = new CancellationTokenSource();
+
+        Thread SimpleNumSearch = null;
+        Thread FibonacciNumSearch = null;
+
+        CancellationToken token1;
+        CancellationToken token2;
+
+        int tmplast = 1, tmpold = 1;
+        int tmplast2 = 1;
+
+        public MainWindow()
         {
-            if (number == 0)
-                return 0;
-            if (number == 1)
-                return 1; 
-            return fibonacci(number - 1) + fibonacci(number - 2);
+            InitializeComponent();
+
+            token1 = cts.Token;
+            token2 = cts2.Token;
+
+            SimpleNumSearch = new Thread(AddSimpleNumbers);
+            FibonacciNumSearch = new Thread(AddFibonacciNumbers);
         }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            PauseSimple.IsEnabled = true;
+            ResumeSimple.IsEnabled = true;
+
+            cts.Cancel();
+            cts2.Cancel();
+
+            cts = new CancellationTokenSource();
+            cts2 = new CancellationTokenSource();
+
+            token1 = cts.Token;
+            token2 = cts2.Token;
+
+            SimpleNumSearch = new Thread(AddSimpleNumbers);
+            FibonacciNumSearch = new Thread(AddFibonacciNumbers);
+
+            
+            SimpleNumSearch.IsBackground = false;
+            FibonacciNumSearch.IsBackground = false;
+
+            tmplast = 1; tmpold = 1;
+
+
+            FibonacciNumbers.Items.Clear();
+            SimpleNumbers.Items.Clear();
+
+            SimpleNumSearch.Start(new KeyValuePair<KeyValuePair<int, int>, CancellationToken>(new KeyValuePair<int, int>(Convert.ToInt32(RightNum.Text), Convert.ToInt32(LeftNum.Text)), token1));
+            FibonacciNumSearch.Start(new KeyValuePair<int, CancellationToken>(tmplast, token2));
+        }
+
+        private void AddSimpleNumbers(object a)
+        {
+            var tmp = (KeyValuePair<KeyValuePair<int, int>, CancellationToken>)a;
+            var tmptoken = tmp.Value;
+            int r = tmp.Key.Key, l = tmp.Key.Value;
+
+            while (tmptoken.IsCancellationRequested == false)
+            {
+                for (int i = r; i <= l; i++)
+                {
+                    if (tmptoken.IsCancellationRequested == true) break;
+                    if (isSimple(i))
+                    {
+                        Application.Current.Dispatcher.Invoke(new Action(() =>
+                        {
+                            SimpleNumbers.Items.Add(i);
+                        }));
+
+                    }
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+
+        private void AddFibonacciNumbers(object a)
+        {
+            var tmp = (KeyValuePair<int, CancellationToken>)a;
+            tmplast = tmp.Key;
+            while (tmp.Value.IsCancellationRequested == false)
+            {
+                Application.Current.Dispatcher.Invoke(new Action(() =>
+                {
+                    FibonacciNumbers.Items.Add(tmplast);
+                    tmplast = GetFibonacci();
+                }));
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            PauseSimple.IsEnabled = false;
+            ResumeSimple.IsEnabled = false;
+
+            cts.Cancel();
+        }
+
+
+        private int GetFibonacci()
+        {
+            int sum = tmpold + tmplast;
+            tmpold = tmplast;
+            return sum;
+        }
+
+
+        private void StopFibonacci_Click(object sender, RoutedEventArgs e)
+        {
+            PauseFibonacci.IsEnabled = false;
+            ResumeFibonacci.IsEnabled = false;
+
+            cts2.Cancel();
+        }
+
+        private void ResumeSimple_Click(object sender, RoutedEventArgs e)
+        {
+            cts = new CancellationTokenSource();
+            token1 = cts.Token;
+
+            ++tmplast;
+
+            SimpleNumSearch = new Thread(AddSimpleNumbers);
+            SimpleNumSearch.Start(new KeyValuePair<KeyValuePair<int, int>, CancellationToken>(new KeyValuePair<int, int>(tmplast, Convert.ToInt32(LeftNum.Text)), token1));
+        }
+
+        private void ResumeFibonacci_Click(object sender, RoutedEventArgs e)
+        {
+            cts2 = new CancellationTokenSource();
+            token2 = cts2.Token;
+
+            FibonacciNumSearch = new Thread(AddFibonacciNumbers);
+            FibonacciNumSearch.Start(new KeyValuePair<int, CancellationToken>(tmplast2, token2));
+        }
+
+        private void PauseSimple_Click(object sender, RoutedEventArgs e)
+        {
+            cts.Cancel();
+        }
+
+        private void PauseFibonacci_Click(object sender, RoutedEventArgs e)
+        {
+            tmplast2 = (int)FibonacciNumbers.Items[FibonacciNumbers.Items.Count-1];
+            cts2.Cancel();
+        }
+
         public static bool isSimple(int x)
         {
             if (x > 1)
@@ -62,87 +186,6 @@ namespace WpfApp3
             }
             else return true;
         }
-
-        CancellationTokenSource cts = new CancellationTokenSource();
-
-        bool stopped = false;
-
-        Thread SimpleNumSearch = null;
-        
-        List<string> listofSimple = new List<string>();
-
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-
-        private void Start_Click(object sender, RoutedEventArgs e)
-        {
-            SimpleNumSearch = new Thread(SProcess);
-            SimpleNumSearch.Priority = ThreadPriority.Highest;
-            SimpleNumSearch.IsBackground = false;
-            SimpleNumSearch.Start(new Alpha(new KeyValuePair<int, int>(Convert.ToInt32(RightNum.Text), Convert.ToInt32(LeftNum.Text)), cts.Token));
-        }
-
-        private void SProcess(object a)
-        {
-            var tmp = (Alpha)a;
-            var tmptoken = tmp.Token;
-
-            int r = tmp.Nums.Key, l = tmp.Nums.Value;
-            
-            for (int i = r; i <= l; i++)                            
-            {
-                if (isSimple(i))
-                {
-                    tmptoken.ThrowIfCancellationRequested();
-                    Application.Current.Dispatcher.Invoke(new Action(() =>
-                    {
-                        SimpleNumbers.Items.Add(i.ToString());
-                    }));
-                }
-                Thread.Sleep(1000);
-            }
-            
-        }
-
-        private void Stop_Click(object sender, RoutedEventArgs e)
-        {
-            SimpleNumSearch = new Thread(SProcess);
-            unsafe
-            {
-                try
-                {
-                    cts.Cancel();
-                }
-                catch (OperationCanceledException ex)
-                {
-                    MessageBox.Show("Thread was cancelled");
-                }
-                catch (ThreadInterruptedException ee)
-                {
-                    MessageBox.Show("Thread was cancelled");
-                }
-                finally
-                {
-                    MessageBox.Show("Thread was cancelled");
-                }
-            }
-            //if (stopped == false)
-            //{
-            //    Application.Current.Dispatcher.Invoke(new Action(() => {
-            //        SimpleNumSearch.Suspend();
-            //    }));
-            //    stopped = true;
-            //}
-            //else
-            //{
-            //    Application.Current.Dispatcher.Invoke(new Action(() => {
-            //    }));
-            //    stopped = false;
-            //}
-        }
-        
 
     }
 }
